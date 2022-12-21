83254d624ae7f2f2fdc3e6bda75ddd37d7c6ae52
Frederico F Silva <fredericoffs@live.com>
2022-12-21 20:48:18 -0300

    Course, Offer

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -15,3 +15,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Sandubas Java café','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1iHNZ4e-FSOhuYr9Fqsjvl2ETPE7VrM-Img&usqp=CAU','https://live.staticflickr.com/5647/21807303085_34c998a42a_z.jpg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2023-01-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-01-13T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2023-02-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-02-13T03:00:00Z', 1);
