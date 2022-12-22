5fc0017f89afbcf88984222670c564a59f2449d1
Frederico F Silva <fredericoffs@live.com>
2022-12-22 17:56:58 -0300

    Section

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -22,3 +22,7 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML','Trilha principal do curso', 1, 'https://live.staticflickr.com/5647/21807303085_34c998a42a_z.jpg', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum','Tire suas dúvidas', 2, 'https://live.staticflickr.com/5647/21807303085_34c998a42a_z.jpg', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives','Lives exclusivas', 3, 'https://live.staticflickr.com/5647/21807303085_34c998a42a_z.jpg', 0, 1);
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste cap vamos começar', 1,'https://live.staticflickr.com/5647/21807303085_34c998a42a_z.jpg', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste cap vamos continuar', 2,'https://live.staticflickr.com/5647/21807303085_34c998a42a_z.jpg', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste cap vamos finalizar', 3,'https://live.staticflickr.com/5647/21807303085_34c998a42a_z.jpg', 1, 2);
