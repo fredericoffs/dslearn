5afde740b7e2e24fa1e383f8c8101820a5b83db8
Frederico F Silva <fredericoffs@live.com>
2022-12-27 18:13:15 -0300

    Notification

--- backend/src/main/java/com/fredericoffs/dslearn/entities/User.java
+++ backend/src/main/java/com/fredericoffs/dslearn/entities/User.java
@@ -3,6 +3,8 @@ package com.fredericoffs.dslearn.entities;
 import static javax.persistence.FetchType.EAGER;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import javax.persistence.Entity;
@@ -12,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -30,6 +33,9 @@ public class User implements Serializable {
       joinColumns = @JoinColumn(name = "user_id"),
       inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles;
+
+  @OneToMany(mappedBy = "user")
+  private List<Notification> notifications = new ArrayList<>();
 
   public User() {
   }
